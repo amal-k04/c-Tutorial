0a5bae6843ac0c03f908d5c67e54f8919d99385e
AMAL K PHILIP <amal17philip@gmail.com>
2025-02-20 23:49:16 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int num,fact,element;
+	printf("Enter the limit:");
+	scanf("%d",&num);
+	element=num;
+	if (0<=num)
+	{
+	fact=1;
+		while(num>0)
+		{	
+			fact=fact*num;
+			num--;	
+		}
+	printf("FACTORIAL OF %d is %d",element,fact);
+	}
+	else
+	{
+		printf("NUMBER SHOULD BE A POSTIVE NUMBER");
+	}
+	return 0;	
+}
