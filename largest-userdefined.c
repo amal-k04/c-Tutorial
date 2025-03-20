7618bd317881aaebbd43be04290d9dd699552ceb
AMAL K PHILIP <amal17philip@gmail.com>
2025-03-20 23:29:58 +0530

    largest-userdefined.c

---
+++ largest-userdefined.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void largest(int arr[],int size){
+    int largest=arr[0];
+    for (int j=1;j<size;j++){
+        if (arr[j]>largest){
+            largest=arr[j];
+            
+        }
+        
+    }
+    printf("The largest element among the list is %d",largest);
+}
+int main(){
+    
+    int size,i;
+    printf("Enter the size of array:");
+    scanf ("%d",&size);
+    int arr[size];
+    for (i=0;i<size;i++){
+        printf("Enter the element");
+        scanf("%d",arr[i]);
+    }
+    largest(arr,size);
+    return 0;
+}
