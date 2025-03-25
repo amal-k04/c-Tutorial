80800f28581ef791ca82b1f0f97d2332e1bf8e85
AMAL K PHILIP <amal17philip@gmail.com>
2025-03-25 16:15:40 +0530

    Add files via upload

---
+++ macro_circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#define pi 3.14159
+#define area_circle(x) (pi)*(x)*(x)
+
+int main(){
+	float x,;
+	printf("Enter the Radius of the circle :");
+	scanf("%f",&x);
+	printf("The Area of the Circle is %.2f\n",area_circle(x));
+	return 0;
+	}
